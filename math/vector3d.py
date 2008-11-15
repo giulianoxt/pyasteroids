7f560a81e020931dc372b09837e1e409b4c0c0cd
Max Rosan <m.r650200@gmail.com>
2008-11-15 03:09:48 +0000

    Classe para manipular vetores

---
+++ math/vector3d.py
@@ -0,0 +1,53 @@
+# Version 0.1
+# 15/11/2008
+
+
+class Vector3d:
+	
+	def __init__(self, x_val, y_val, z_val):
+		self.x = x_val
+		self.y = y_val
+		self.z = z_val
+	
+	# length of vector
+	def get_mod(self):
+		return sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
+	# addition
+	def __add__(self, vec3d):
+		return Vector3d(self.x + vec3d.x, self.y + vec3d.y, self.z + vec3d.z)
+	
+	# subtraction
+	def __sub__(self, vec3d):
+		return Vector3d(self.x - vec3d.x, self.y - vec3d.y, self.z - vec3d.z)
+	
+	# dot scalar
+	def __mult__(self, vec3d):
+		return (self.x*vec3d.x + self.y*vec3d.y + self.z*vec3d.z)
+	
+	# cross product
+	def cross_product(self, vec3d):
+		x_cp = self.y*vec3d.z - self.z*vec3d.y
+		y_cp = self.z*vec3d.x - self.x*vec3d.z
+		z_cp = self.x*vec3d.y - self.y*vec3d.x
+		return Vector3d(x_cp, y_cp, z_cp)
+	
+	# returns angle ( radians ) two vectors
+	def angle(self, vec3d):
+		return acos( (self*vec3d)/(self.get_mod()*vec3d.get_mod()) )
+	
+	# scalar multiplication
+	def __mult__(self, k):
+		return Vector3d(self.x*k, self.y*k, self.z*k)
+	
+	# returns a unitary vector
+	def normalizing(self):
+		tmp_mod = self.get_mod()
+		return Vector3d(self.x/tmp_mod, self.y/tmp_mod, self.z/tmp_mod)
+	
+	# angle ( radians ) between the positive x-axis and vector projected onto the xy-plane
+	def angle_phi(self):
+		return atan2(self.y, self.x)
+	
+	# angle ( radians ) between the positive z-axis and the vector
+	def angle_theta(self):
+		return atan2(sqrt(self.x*self.x + self.y*self.y), self.z)
